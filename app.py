12eaf809f43e795a41fc8d4d98a4920881742200
Bhavesh <thakurbhavesh275@gmail.com>
2026-07-17 19:44:53 +0530

    Updated AI Symptom Checker

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from google import genai
 # Gemini API
 # -----------------------------
 client = genai.Client(
-    api_key=st.secrets["GEMINI_API_KEY"]
+    api_key=st.secrets["GEMINI_API_KEY]
 )
 
 # -----------------------------
@@ -143,3 +143,4 @@ elif selected == "About":
 Bhavesh Thakur
             
 """)
+
