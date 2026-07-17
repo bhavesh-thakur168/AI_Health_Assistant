3cccc724cf86af05a4b3a9927261204e883782b6
Bhavesh <thakurbhavesh275@gmail.com>
2026-07-17 21:22:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from google import genai
 import streamlit as st
 
-client = genai.Client(
-    api_key=st.secrets["GEMINI_API_KEY"]
-)
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
 # -----------------------------
 # Page Settings
